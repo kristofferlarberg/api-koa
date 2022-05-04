b3ed7ce29cf389415210511b633c94aed099227d
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-05-04 13:27:07 +0200

    chore: add mongodb

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const body = require("koa-body");
 
 const app = new Koa();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_db');
+
+const noteSchema = mongoose.Schema({
+  id: Number,
+  content: String,
+});
+
+var Note = mongoose.model("Note", noteSchema);
+
 app.use(body());
-
 const routes = require('./routes.js');
-
 app.use(routes.routes());
 
 app.listen(3000);
